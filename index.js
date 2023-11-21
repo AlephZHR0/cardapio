85daca03e2cb5ab3a1df8d85a908682767f9b22d
AlephZHR0 <jpjoaopedro1011@gmail.com>
2023-11-21 12:57:36 -0300

    funcionalidade do botão finalizar implementada, correções de bugs e melhorias na visualização

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function aumenta(id) {
     var element = document.getElementById(id);
-    var value = parseInt(element.value);
+    var value = parseFloat(element.value);
     if (isNumeric(value)) {
         value++;
         element.value = value;
@@ -12,7 +12,7 @@ function aumenta(id) {
 
 function diminui(id) {
     var element = document.getElementById(id);
-    var value = parseInt(element.value);
+    var value = parseFloat(element.value);
     if (value > 0 && isNumeric(value)) {
         value--;
         element.value = value;
@@ -21,9 +21,39 @@ function diminui(id) {
         element.value = value;
     }
 }
+
 function isNumeric(value) {
     if (!isNaN(value)) {
         return true;
     }
     return false;
+}
+
+function finalizarPedido() {
+    const whatsappEndpoint = "https://wa.me/";
+    const numero = "5511967405413";
+    const parametro = "/?text="
+    let mensagemInicial = "Olá, tudo bem?\nSegue meu pedido:";
+    let itensPedidos = "";
+    let valorFinal = 0;
+    let listaDeItens = document.getElementsByClassName("item")
+    for (var i = 0; i < listaDeItens.length; i++) {
+        let item = listaDeItens[i];
+        quantidade = item.getElementsByTagName("input")[0].value;
+        if (quantidade > 0) {
+            nome = item.getElementsByClassName("nome-item")[0].textContent;
+            let preco = item.getElementsByClassName("preco-item")[0].textContent;
+            preco = parseFloat(preco.replace(/[^\d,]/g, '').replace(',', '.'));
+            valorFinal += preco * quantidade;
+            itensPedidos += `\n- ${quantidade} ${nome};`;
+        }
+    }
+    if (itensPedidos == "") {
+        alert("Você não selecionou nenhum item!");
+        return;
+    }
+    valorFinal = `\n----------------------\nValor Final: R$${valorFinal.toFixed(2)}`
+    let mensagemFinal = whatsappEndpoint + numero + parametro + encodeURIComponent(mensagemInicial + itensPedidos + valorFinal);
+    // console.log(mensagemFinal);
+    window.open(mensagemFinal);
 }
